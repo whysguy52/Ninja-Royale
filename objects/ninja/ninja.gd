5950d986f2d64e38bacbdbd018ac9b92e638190a
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-04-29 21:58:04 -0500

    adds ninja run, dash animations

--- objects/ninja/ninja.gd
+++ objects/ninja/ninja.gd
@@ -11,18 +11,18 @@ const MAX_SLOPE_ANGLE = 40
 const MOUSE_SENSITIVITY = 0.05
 const DASH_SPEED = 30
 const DASH_ACCEL = 15
+const BLEND_SPEED = 6.9
+const BLEND_SPEED_DASH = 17
 
+var ninja_blend = preload("res://assets/3d/ninja/blend/ninja.blend")
 var dir = Vector3()
 var can_dash = true
 var can_jump = true
 var is_dashing = false
 var can_climb_ladder = false
 
-var ninja_blend = preload("res://assets/3d/ninja/blend/ninja.blend")
-
 func _ready():
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
   adjust_ninja_material()
 
 
@@ -36,7 +36,7 @@ func adjust_ninja_material():
 
 func _physics_process(delta):
   process_menu()
-  process_input()
+  process_input(delta)
   process_movement(delta)
 
 
@@ -45,9 +45,9 @@ func process_menu():
     $game_menu.show()
 
 
-func process_input():
+func process_input(delta):
   process_dash()
-  process_walk_direction()
+  process_walk_direction(delta)
   process_jump()
 
 
@@ -60,7 +60,7 @@ func process_dash():
     $dash_timer.start()
 
 
-func process_walk_direction():
+func process_walk_direction(delta):
   dir = Vector3()
   var cam_xform = $rotation/camera.get_global_transform()
   var input_movement_vector = Vector2()
@@ -83,13 +83,31 @@ func process_walk_direction():
   if Input.is_action_pressed("strafe_right"):
     input_movement_vector.x += 1
 
+  input_movement_vector = input_movement_vector.normalized()
+  dir += -cam_xform.basis.z.normalized() * input_movement_vector.y
+  dir += cam_xform.basis.x.normalized() * input_movement_vector.x
+
+  $animation_tree.set("parameters/TimeScale/scale", 1.0 if input_movement_vector.y >= 0 else -1.0)
+
   if is_dashing and input_movement_vector == Vector2.ZERO:
     is_dashing = false
     $dash_timer.stop()
 
-  input_movement_vector = input_movement_vector.normalized()
-  dir += -cam_xform.basis.z.normalized() * input_movement_vector.y
-  dir += cam_xform.basis.x.normalized() * input_movement_vector.x
+    var blend_amount = $animation_tree.get("parameters/blend_arms/blend_amount")
+    $animation_tree.set("parameters/blend_arms/blend_amount", lerpf(blend_amount, 0.0, delta * BLEND_SPEED))
+  elif is_dashing:
+    var blend_amount = $animation_tree.get("parameters/blend_arms/blend_amount")
+    $animation_tree.set("parameters/blend_arms/blend_amount", lerpf(blend_amount, 1.0, delta * BLEND_SPEED_DASH))
+  elif not is_dashing:
+    var blend_amount = $animation_tree.get("parameters/blend_arms/blend_amount")
+    $animation_tree.set("parameters/blend_arms/blend_amount", lerpf(blend_amount, 0.0, delta * BLEND_SPEED))
+
+  if input_movement_vector.y != 0:
+    var blend_amount = $animation_tree.get("parameters/blend_idle_run/blend_amount")
+    $animation_tree.set("parameters/blend_idle_run/blend_amount", lerpf(blend_amount, 1.0, delta * BLEND_SPEED))
+  elif input_movement_vector.y == 0:
+    var blend_amount = $animation_tree.get("parameters/blend_idle_run/blend_amount")
+    $animation_tree.set("parameters/blend_idle_run/blend_amount", lerpf(blend_amount, 0.0, delta * BLEND_SPEED))
 
 
 func process_jump():
@@ -138,7 +156,7 @@ func _input(event):
     var invert_vert = Settings.controls["camera"]["invert_vert"] if Settings.controls["camera"]["invert_vert"] != null else false
     var invert_horz = Settings.controls["camera"]["invert_horz"] if Settings.controls["camera"]["invert_horz"] != null else false
     var vert_val = -1 if invert_vert else 1
-    var horz_val = -1 if invert_horz else 1
+    var horz_val = 1 if invert_horz else -1
 
     $rotation.rotate_x(deg_to_rad(event.relative.y * MOUSE_SENSITIVITY * vert_val))
     self.rotate_y(deg_to_rad(event.relative.x * MOUSE_SENSITIVITY * horz_val))
